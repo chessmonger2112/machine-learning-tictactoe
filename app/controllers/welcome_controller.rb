db03bad79e41db23ee93c99d9a7aadb1e28d639c
kyle smith <chessmonger2112@yahoo.com>
2015-11-23 12:11:42 -0600

    ajax sent as objects in array

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,25 +1,21 @@
 class WelcomeController < ApplicationController
     def index
       @his = History.all
-      @hiss = History.new
+      puts "$$$"
     end
 
     def del
       lost_position = params[:position]
-      # lost_position = "QQQQQQQQQ"
       lost_array = History.where(move_history: lost_position)
       lost_array.delete_all
     end
 
   def prev
-    position_array = params[:positions]
-    # for index in 0..position_array.length-1
-    (0..position_array.length-1).step(2) do |index|
+    params[:positions].each do |current_obj|
+      history = current_obj[1]
       @his = History.new
-      even_element = position_array[index]
-      odd_element = position_array[index + 1]
-      @his.move_history = even_element
-      @his.last_move = odd_element
+      @his.move_history = history[:move_history]
+      @his.last_move = history[:last_move]
       @his.save
     end
   end
@@ -38,6 +34,5 @@ class WelcomeController < ApplicationController
     end
     def show_database
       @his = History.all
-      "heyyyyyyyyy"
     end
 end
